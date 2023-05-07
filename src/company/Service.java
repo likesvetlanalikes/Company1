06c17e3af88113686a081b3985eed832b773c095
svetlana <svetlana_040285@mail.ru>
2023-05-07 09:53:23 +0300

    Company

---
+++ src/company/Service.java
@@ -0,0 +1,14 @@
+package company;
+
+public class Service implements FindeInterface{
+
+    public boolean finde(Employeer employeer, String name){
+        Employeer[] employeers = employeer.getEmployees();
+        for (Employeer empl : employeers){
+            String emplName = empl.name;
+            if(emplName.equals(name)) {
+                return true;
+            }
+        }return false;
+    }
+}
